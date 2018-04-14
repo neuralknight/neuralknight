51bc62dc01bc1daee02dd4f357ca5440efcf68a6
Dave Snowberger <git@fartherout.org>
2018-04-14 14:01:23 -0700

    implemented trainer based on simple classification algorithm

---
+++ trainer.py
@@ -0,0 +1,19 @@
+import sklearn
+
+def train_classifier(x_train, y_train):
+    '''Trains classifier given a set of features and corresponding labels
+
+    Inputs:
+        x_train: board states of a given game, represented as features in
+                 ndarray format
+        y_train: list of labels associated with x_train's states, represented
+                 as an m x 1 ndarray, where m is the number of board states in x
+    Outputs:
+        classifier: An updated classifier
+
+    '''
+
+    gnb = sklearn.naive_bayes.GaussianNB()
+    classifier = gnb.fit(x_train, y_train)
+
+    return(classifier)
