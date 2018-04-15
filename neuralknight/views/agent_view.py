e77ed919446900d6add6a59cf3fdbef3ea9ead55
Dave Snowberger <git@fartherout.org>
2018-04-15 15:01:56 -0700

    fixed typo

--- neuralknight/views/agent_view.py
+++ neuralknight/views/agent_view.py
@@ -1,6 +1,6 @@
 import requests
 from pyramid.view import view_config
-from ../models/board_evaluation import evaluate_boards
+from ..models/board_evaluation import evaluate_boards
 
 @view_config(route_name="agent")
 def agent_view(request):
