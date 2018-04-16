47d92b67d543292d59ccd18e3095945978d2acb9
Dave Snowberger <git@fartherout.org>
2018-04-16 12:55:31 -0700

    test fix

--- neuralknight/tests/test_agent_view.py
+++ neuralknight/tests/test_agent_view.py
@@ -1,6 +1,6 @@
 import pytest
 from pyramid.httpexceptions import HTTPBadRequest
-from neuralknight.views import agent
+from neuralknight.views.agent import issue_agent_view, agent_view
 
 def test_issue_agent_bad_request(dummy_post_request):
     '''Test request without proper query string'''
