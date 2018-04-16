e2a3346f1c62f1ca93649dfe4c21e4f264900d78
Dave Snowberger <git@fartherout.org>
2018-04-16 10:50:31 -0700

    fix typo

--- neuralknight/tests/test_agent_view.py
+++ neuralknight/tests/test_agent_view.py
@@ -3,6 +3,6 @@ from ..views import agent
 from pyramid.httpexceptions import HTTPBadRequest
 
 
-test_issue_agent_bad_request(dummy_post_request):
+def test_issue_agent_bad_request(dummy_post_request):
     '''Test request without proper query string'''
     assert isinstance(issue_agent_view(dummy_post_request), HTTPBadRequest)
