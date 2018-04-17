1ffd25eada350733b25a9f8392671af8158f1b4d
Shannon Tully <scatman.st@gmail.com>
2018-04-17 11:25:00 -0700

    ui has basic functionality, waiting on bug fixes game side

--- neuralknight/views/agent.py
+++ neuralknight/views/agent.py
@@ -5,6 +5,7 @@ from pyramid.response import Response
 from pyramid.view import view_config
 
 from ..models import Agent
+from ..models import UserAgent
 
 
 @view_config(route_name='issue_agent', request_method='POST', renderer='json')
@@ -13,13 +14,22 @@ def issue_agent_view(request):
         game_id = request.POST['id']
     except KeyError:
         return HTTPBadRequest()
-
+    if 'user' in request.POST:
+        return Response(body=json.dumps({'agent_id': UserAgent(game_id).agent_id}), status_code=200)
     return Response(body=json.dumps({'agent_id': Agent(game_id).agent_id}), status_code=200)
 
 
-@view_config(route_name='agent', request_method='PUT', renderer='json')
+@view_config(route_name='agent', request_method=('PUT', 'GET'), renderer='json')
 def agent_view(request):
     agent_id = request.matchdict['agent_id']
-    Agent.AGENT_POOL[agent_id].play_round()
+    agent = Agent.AGENT_POOL[agent_id]
+
+    if request.method == 'GET':
+        assert isinstance(agent, UserAgent)
+    else:
+        if isinstance(agent, UserAgent):
+            agent.play_round(request.matchdict['move'])
+        else:
+            agent.play_round()
 
     return Response()
