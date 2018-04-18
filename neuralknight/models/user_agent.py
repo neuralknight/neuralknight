35c7f2156b2916f4f2443f8bf2905ceba72306eb
Adam Grandquist <grandquista@gmail.com>
2018-04-18 09:53:24 -0700

    Make move against current board state.

--- neuralknight/models/user_agent.py
+++ neuralknight/models/user_agent.py
@@ -8,14 +8,10 @@ class UserAgent(BaseAgent):
         super().__init__(game_id, player)
         self.request('POST', '/issue-agent', json={'id': game_id, 'player': 2})
 
-    def join_game(self):
-        super().join_game()
-        self.state = self.get_state()
-
     def play_round(self, move):
         if move is None:
             return
-        proposal = self.state
+        proposal = self.get_state()
         proposal[move[1][0]][move[1][1]] = proposal[move[0][0]][move[0][1]]
         proposal[move[0][0]][move[0][1]] = 0
         self.put_board(proposal)
