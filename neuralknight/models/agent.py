6d9725c2d40ad5d984e1d9405916dffd4f536fa4
Dave Snowberger <git@fartherout.org>
2018-04-18 14:37:57 -0700

    modified get boards

--- neuralknight/models/agent.py
+++ neuralknight/models/agent.py
@@ -5,28 +5,28 @@ from random import randint
 class Agent(BaseAgent):
     '''Computer Agent'''
 
-    def get_boards(self, cursor):
+    def get_boards(self, cursor=None):
         '''Retrieves potential board states'''
-        params = {'lookahead': self.lookahead, 'cursor': cursor}
+        params = {'lookahead': self.lookahead}
+        if cursor:
+            params['cursor'] = cursor
         data = self.request('GET', '/v1.0/games/{}/states'.format(self.game_id), params=params)
         return {'boards': data['boards'], 'cursor': data['cursor']}
 
     def play_round(self):
         '''Play a game round'''
-        cursor = 'first'
-        board_score = 0
-        best_boards = []
-        while cursor:
+        board_options = self.get_boards()
+        evaluation = self.evaluate_boards(board_options['boards'])
+        board_score = evaluation['board_score']
+        best_boards = [evaulation['best_board']]
+        while board_options['cursor']:
             board_options = self.get_boards(cursor)
-            boards = board_options['boards']
-            cursor = board_options['cursor']
-            evaluation = self.evaluate_boards(boards)
+            evaluation = self.evaluate_boards(board_options['boards'])
             if evaluation['board_score'] > board_score:
                 best_boards = [evaluation['best_board']]
             elif evaluation['board_score'] == board_score:
                 if evaluation['best_board'] not in best_boards:
                     best_boards.append(evaluation['best_board'])
-
         return self.put_board(best_boards[randint(0,len(best_boards)-1)])
 
     def play_game(self):
