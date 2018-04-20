1aa733a26ad103c3e40f7c135ad307be6234e2d4
Dave Snowberger <git@fartherout.org>
2018-04-19 19:57:49 -0700

    lookaheads back to 4

--- neuralknight/scripts/cli.py
+++ neuralknight/scripts/cli.py
@@ -68,7 +68,7 @@ class CLIAgent(Cmd):
         ).json()['agent_id']
         requests.post(
             f'{ self.api_url }/issue-agent-lookahead',
-            json={'id': self.game_id, 'player': 2, 'lookahead': 1})
+            json={'id': self.game_id, 'player': 2, 'lookahead': 4})
         print_board(format_board(get_info(self.api_url, self.game_id)))
 
     def do_piece(self, arg_str):
