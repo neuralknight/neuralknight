f9a5d94f3db2870882f3a1587157fb55ca5e152b
Adam Grandquist <grandquista@gmail.com>
2018-07-22 16:35:12 -0700

    Write tests for main.

--- main.go
+++ main.go
@@ -24,35 +24,38 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
+	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 const connStr = "postgres://pqgotest:password@localhost/pqgotest?sslmode=verify-full"
 
-func main() {
-	db, err := gorm.Open("postgres", connStr)
+func shutdown(srv *http.Server, sigint <-chan os.Signal, idleConnsClosed chan<- struct{}) {
+	<-sigint
+
+	// We received an interrupt signal, shut down.
+	if err := srv.Shutdown(context.Background()); err != nil {
+		// Error from closing listeners, or context timeout:
+		log.Printf("HTTP server Shutdown: %v", err)
+	}
+}
+
+// Main interruptable process.
+func Main(sigint <-chan os.Signal, idleConnsClosed chan<- struct{}) {
+	defer close(idleConnsClosed)
+
+	var srv http.Server
+
+	go shutdown(&srv, sigint, idleConnsClosed)
+
+	db, err := gorm.Open("sqlite3", "chess.db")
 	if err != nil {
-		log.Panicln("failed to connect database")
+		log.Panicln("failed to connect database", err, connStr)
 	}
 
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
 	db.DB().SetConnMaxLifetime(time.Hour)
-
-	var srv http.Server
-
-	idleConnsClosed := make(chan struct{})
-	go func() {
-		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt)
-		<-sigint
-
-		// We received an interrupt signal, shut down.
-		if err := srv.Shutdown(context.Background()); err != nil {
-			// Error from closing listeners, or context timeout:
-			log.Printf("HTTP server Shutdown: %v", err)
-		}
-		close(idleConnsClosed)
-	}()
 
 	srv.Handler = Handler{}
 
@@ -60,6 +63,12 @@ func main() {
 		// Error starting or closing listener:
 		log.Printf("HTTP server ListenAndServe: %v", err)
 	}
+}
 
+func main() {
+	sigint := make(chan os.Signal, 1)
+	idleConnsClosed := make(chan struct{})
+	signal.Notify(sigint, os.Interrupt)
+	go Main(sigint, idleConnsClosed)
 	<-idleConnsClosed
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestInteruptMain(t *testing.T) {
+	sigint := make(chan os.Signal, 1)
+	idleConnsClosed := make(chan struct{})
+	go Main(sigint, idleConnsClosed)
+	sigint <- os.Interrupt
+	close(sigint)
+	<-idleConnsClosed
+}
+
+func TestMain(t *testing.T) {
+	go main()
+}
